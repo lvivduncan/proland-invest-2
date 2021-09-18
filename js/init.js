d0790b2c687fe8c6df51d438d78e5098c76cde44
lvivduncan <lvivduncan@gmail.com>
2021-09-18 13:44:52 +0300

    add levus-up button

--- js/init.js
+++ js/init.js
@@ -536,4 +536,31 @@
     }
 }
 
-// 8-09-2021
+// 18-09-2021
+{
+    const levusUp = document.createElement('div');
+    levusUp.setAttribute('id', 'levus-up');
+    document.body.append(levusUp);
+
+    // show/hide
+    window.addEventListener('scroll', () => {
+        if(window.pageYOffset < 50){
+            levusUp.className = '';
+        } else if(window.pageYOffset > 100) {
+            setTimeout( () => {
+                levusUp.className = 'active';
+            }, 10);
+        }
+    });
+
+    // click to up
+    levusUp.addEventListener('click', () => {
+        document.documentElement.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
+// 18-09-2021
